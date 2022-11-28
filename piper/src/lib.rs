341ac5cac1969df3807f9622b17e5384a3e74020
Chen Xu <windoze@0d0a.com>
2022-11-28 14:57:43 +0800

    Restructure

---
+++ piper/src/lib.rs
@@ -0,0 +1,70 @@
+use std::{collections::HashMap, fmt::Debug};
+
+use clap::Parser;
+use pipeline::{ErrorCollectingMode, ErrorRecord};
+use serde::{Deserialize, Serialize};
+
+mod common;
+mod pipeline;
+mod piper;
+
+pub use common::{Appliable, Logged};
+pub use pipeline::{PiperError, Value, ValueType};
+pub use piper::Piper;
+
+#[derive(Parser, Debug, Clone)]
+#[command(author, version, about, long_about = None)]
+pub struct Args {
+    /// Pipeline definition file name
+    #[arg(short, long, env = "PIPELINE_DEFINITION_FILE")]
+    pub pipeline: String,
+
+    /// Lookup source definition file name
+    #[arg(short, long, env = "LOOKUP_DEFINITION_FILE")]
+    pub lookup: String,
+
+    #[arg(long, default_value = "0.0.0.0", env = "LISTENING_ADDRESS")]
+    pub address: String,
+
+    #[arg(long, default_value_t = 8000, env = "LISTENING_PORT")]
+    pub port: u16,
+
+    #[arg(long, default_value_t = false, env = "ENABLE_MANAGED_IDENTITY")]
+    pub enable_managed_identity: bool,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct SingleRequest {
+    pipeline: String,
+    data: HashMap<String, serde_json::Value>,
+    #[serde(default)]
+    validate: bool,
+    #[serde(default)]
+    errors: ErrorCollectingMode,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Request {
+    requests: Vec<SingleRequest>,
+}
+
+#[derive(Debug, Serialize)]
+pub struct SingleResponse {
+    pipeline: String,
+    status: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    time: Option<f64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    count: Option<usize>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    data: Option<Vec<HashMap<String, serde_json::Value>>>,
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    errors: Vec<ErrorRecord>,
+}
+
+#[derive(Debug, Serialize)]
+pub struct Response {
+    results: Vec<SingleResponse>,
+}
---
+++ piper/src/piper.rs
@@ -0,0 +1,195 @@
+use std::{collections::HashMap, time::Instant};
+
+use azure_core::auth::TokenCredential;
+use azure_identity::{DefaultAzureCredential, DefaultAzureCredentialBuilder};
+use futures::future::join_all;
+use tracing::{debug, instrument};
+
+use crate::{
+    pipeline::{dump_lookup_sources, ErrorCollector, Pipeline, PiperError, ValidationMode, Value},
+    Appliable, Args, Logged, Request, Response, SingleRequest, SingleResponse,
+};
+
+#[derive(Debug)]
+pub struct Piper {
+    pub(crate) pipelines: HashMap<String, Pipeline>,
+}
+
+impl Piper {
+    pub async fn new(args: Args) -> Result<Self, PiperError> {
+        let pipeline_def = load_file(&args.pipeline, args.enable_managed_identity).await?;
+        let lookup_def = load_file(&args.lookup, args.enable_managed_identity).await?;
+
+        let mut pipelines = Pipeline::load(&pipeline_def, &lookup_def).log()?;
+        // Use invalid identifier as the name, avoid clashes with user-defined pipelines
+        pipelines.insert("%health".to_string(), Pipeline::get_health_checker());
+        Ok(Self { pipelines })
+    }
+
+    pub async fn health_check(&self) -> bool {
+        let (_, ret) = self
+            .pipelines
+            .get("%health")
+            .unwrap()
+            .process_row(vec![Value::Int(57)], ValidationMode::Strict)
+            .unwrap()
+            .eval()
+            .await;
+        if (ret.len() == 1) && (ret[0].len() == 2) {
+            matches!(ret[0][1], Value::Int(99))
+        } else {
+            false
+        }
+    }
+
+    pub fn get_pipelines(&self) -> HashMap<String, serde_json::Value> {
+        self.pipelines
+            .values()
+            .map(|p| (p.name.clone(), p.to_json()))
+            .collect()
+    }
+
+    pub fn get_lookup_sources() -> serde_json::Value {
+        dump_lookup_sources()
+    }
+
+    #[instrument(level = "trace", skip(self))]
+    pub async fn process(&self, req: Request) -> Result<Response, PiperError> {
+        debug!(
+            "Received request, contains {} sub-requests",
+            req.requests.len()
+        );
+        let futures: Vec<_> = req
+            .requests
+            .into_iter()
+            .map(|r| async {
+                let pipeline = r.pipeline.clone();
+                let r = self.process_single_request(r).await;
+                match r {
+                    Ok(r) => r,
+                    Err(e) => SingleResponse {
+                        pipeline,
+                        status: format!("ERROR: {}", e),
+                        time: None,
+                        count: None,
+                        data: None,
+                        errors: vec![],
+                    },
+                }
+            })
+            .collect();
+        let results = join_all(futures).await;
+        Ok(Response { results })
+    }
+
+    #[instrument(level = "trace", skip(self))]
+    async fn process_single_request(
+        &self,
+        req: SingleRequest,
+    ) -> Result<SingleResponse, PiperError> {
+        let pipeline = self
+            .pipelines
+            .get(&req.pipeline)
+            .ok_or_else(|| PiperError::PipelineNotFound(req.pipeline.clone()))?;
+        debug!("Processing request to pipeline {}", pipeline.name);
+
+        let schema = &pipeline.input_schema;
+
+        let row: Vec<Value> = schema
+            .columns
+            .iter()
+            .map(|c| {
+                req.data
+                    .get(c.name.as_str())
+                    .map(|v| Value::from(v.clone()))
+                    .unwrap_or_default()
+            })
+            .collect();
+
+        let now = Instant::now();
+        let (ret, errors) = pipeline
+            .process_row(
+                row,
+                if req.validate {
+                    ValidationMode::Strict
+                } else {
+                    ValidationMode::Lenient
+                },
+            )?
+            .eval()
+            .await
+            .collect_into_json(req.errors);
+        Ok(SingleResponse {
+            pipeline: req.pipeline,
+            status: "OK".to_owned(),
+            time: Some((now.elapsed().as_micros() as f64) / 1000f64),
+            count: Some(ret.len()),
+            data: Some(ret),
+            errors,
+        })
+    }
+}
+
+async fn make_request(
+    url: &str,
+    enable_managed_identity: bool,
+) -> Result<reqwest::RequestBuilder, PiperError> {
+    let client = reqwest::Client::new();
+    if url.starts_with("https://") && url.contains(".blob.core.windows.net/") {
+        // It's on Azure Storage Blob
+        let credential = if enable_managed_identity {
+            DefaultAzureCredential::default()
+        } else {
+            DefaultAzureCredentialBuilder::new()
+                .exclude_managed_identity_credential()
+                .build()
+        };
+        let token = credential
+            .get_token("https://storage.azure.com/")
+            .await
+            .log()
+            .map_err(|e| PiperError::AuthError(format!("{:?}", e)))
+            .map(|t| t.token.secret().to_string())
+            .ok();
+        match token {
+            // Acquired token and use it
+            Some(t) => Ok(client
+                .get(url)
+                // @see: https://learn.microsoft.com/en-us/azure/storage/common/storage-auth-aad-app?tabs=dotnet#create-a-block-blob
+                .header("x-ms-version", "2017-11-09")
+                .bearer_auth(t)),
+            // We don't have token, assume it's public accessible
+            None => Ok(client.get(url)),
+        }
+    } else {
+        Ok(client.get(url))
+    }
+}
+
+async fn load_file(path: &str, enable_managed_identity: bool) -> Result<String, PiperError> {
+    debug!("Reading file at {}", path);
+    Ok(if path.starts_with("http:") || path.starts_with("https:") {
+        let resp = make_request(path, enable_managed_identity)
+            .await?
+            .send()
+            .await
+            .log()
+            .map_err(|e| PiperError::Unknown(e.to_string()))?;
+        resp.text()
+            .await
+            .log()
+            .map_err(|e| PiperError::Unknown(e.to_string()))
+    } else {
+        tokio::fs::read_to_string(path)
+            .await
+            .log()
+            .map_err(|e| PiperError::Unknown(e.to_string()))
+    }?
+    .then(|s| {
+        debug!(
+            "Successfully read file at {}, file length is {}",
+            path,
+            s.len()
+        );
+    }))
+}
