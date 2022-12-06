9fb0f413e624d5932862c1385525abade8ee586d
Chen Xu <windoze@0d0a.com>
2022-12-07 02:00:40 +0800

    Add distance calculation

--- python/examples/json_agg_demo.py
+++ python/examples/json_agg_demo.py
@@ -2,12 +2,14 @@
 # Install the `feathrpiper` package
 #! pip install -U feathrpiper
 
+import asyncio
 import json
 import feathrpiper
 
 
 # Test data:
 # PreviousData.Vehicle[0].DashCam has been changed to "Yes" otherwise the percentage cannot be calculated
+# PreviousData.Postcode has been changed to "NW1 6XE" otherwise the distance is 0
 s = r'''
 {
     "ModelInput": {
@@ -38,7 +40,7 @@ s = r'''
             ]
         },
         "PreviousData": {
-            "Postcode": "BH1 2AT",
+            "Postcode": "NW1 6XE",
             "Vehicle": [
                 {
                     "VehicleNumber": "189",
@@ -52,6 +54,25 @@ s = r'''
     }
 }
 '''
+
+POSTCODE_LOCATIONS = {
+    "BH1 2AT": {
+        "latitude": 50.724,
+        "longitude": -1.9065
+    },
+    "NW1 6XE": {
+        "latitude": 51.5237,
+        "longitude": -0.1585
+    }
+}
+
+async def lookup_loc(postcode, fields):
+    """
+    User-Defined Lookup Function for Postcode to geolocation lookup
+    In real-world case, this could be a call to a 3rd party API, or a database lookup
+    """
+    print("Lookup postcode: ", postcode)
+    return [POSTCODE_LOCATIONS[str(postcode)][f] for f in fields]
 
 
 def demo_agg(s):
@@ -104,16 +125,28 @@ t_dsl(s)
     previous_dash_cam_percentage=double(len(previous_has_dash_cam))/len(previous_all_vehicles),
     previous_driver_count=len(array_distinct(previous_driver_ids))
 | project dash_cam_variation=current_dash_cam_percentage-previous_dash_cam_percentage
+| project
+    current_postcode = string(get_json_object(s, "$.ModelInput.CurrentData.Postcode")),
+    previous_postcode = string(get_json_object(s, "$.ModelInput.PreviousData.Postcode"))
+# Lookup current location
+| lookup current_lat=latitude, current_long=longitude from postcode_locs on current_postcode
+# Lookup previous location
+| lookup previous_lat=latitude, previous_long=longitude from postcode_locs on previous_postcode
+# Calculate distance between current and previous locations
+| project dist = distance(current_lat, current_long, previous_lat, previous_long)
 | project-keep
+    current_lat, current_long, previous_lat, previous_long,
     current_dash_cam_percentage,
     current_driver_count,
     previous_dash_cam_percentage,
     previous_driver_count,
-    dash_cam_variation
+    dash_cam_variation,
+    dist
 ;
 
 # UDF implementation
 t_udf(s)
+# Call UDF to do the extraction and aggregation
 | project ret=demo_agg(s)
 | project
     current_dash_cam_percentage=ret[0],
@@ -121,29 +154,42 @@ t_udf(s)
     previous_dash_cam_percentage=ret[2],
     previous_driver_count=ret[3],
     dash_cam_variation=ret[4]
+| project
+    current_postcode = string(get_json_object(s, "$.ModelInput.CurrentData.Postcode")),
+    previous_postcode = string(get_json_object(s, "$.ModelInput.PreviousData.Postcode"))
+| lookup current_lat=latitude, current_long=longitude from postcode_locs on current_postcode
+| lookup previous_lat=latitude, previous_long=longitude from postcode_locs on previous_postcode
+| project dist = distance(current_lat, current_long, previous_lat, previous_long)
 | project-keep
+    current_lat, current_long, previous_lat, previous_long,
     current_dash_cam_percentage,
     current_driver_count,
     previous_dash_cam_percentage,
     previous_driver_count,
-    dash_cam_variation
+    dash_cam_variation,
+    dist
 ;
 '''
 
 # Piper for local execution
-p = feathrpiper.Piper(pipelines, "", {"demo_agg": demo_agg})
+p = feathrpiper.Piper(pipelines, {"postcode_locs": lookup_loc}, {"demo_agg": demo_agg})
 
-print("Testing DSL implementation...")
-(ret1, errors) = p.process("t_dsl", {"s": s})
-print(json.dumps(ret1[0], sort_keys=True, indent=4))
-# There shouldn't be error
-assert (len(errors) == 0)
+# Need to be async as we're using async lookup function
+async def test():
+    print("Testing DSL implementation...")
+    (ret1, errors) = await p.process_async("t_dsl", {"s": s})
+    print(json.dumps(ret1[0], sort_keys=True, indent=4))
+    # There shouldn't be error
+    assert (len(errors) == 0)
 
-print("Testing UDF implementation...")
-(ret2, errors) = p.process("t_udf", {"s": s})
-print(json.dumps(ret2[0], sort_keys=True, indent=4))
-# There shouldn't be error
-assert (len(errors) == 0)
+    print("Testing UDF implementation...")
+    (ret2, errors) = await p.process_async("t_udf", {"s": s})
+    print(json.dumps(ret2[0], sort_keys=True, indent=4))
+    # There shouldn't be error
+    assert (len(errors) == 0)
 
-# These 2 implementations should do the same thing
-assert (ret1 == ret2)
+    # These 2 implementations should do the same thing
+    assert (ret1 == ret2)
+
+# Run the test
+asyncio.run(test())
