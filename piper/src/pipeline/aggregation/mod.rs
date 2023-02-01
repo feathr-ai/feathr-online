9604ef735c024d4a26121576027d470b6c0990d7
徐辰 <windoze@0d0a.com>
2023-02-02 01:02:37 +0800

    Agg functions formats

--- piper/src/pipeline/aggregation/mod.rs
+++ piper/src/pipeline/aggregation/mod.rs
@@ -74,99 +74,31 @@ impl Aggregation {
     }
 }
 
+#[rustfmt::skip]
 pub fn init_built_in_agg_functions() -> HashMap<String, Box<dyn AggregationFunction>> {
     let mut agg = HashMap::new();
-    agg.insert(
-        "count".to_string(),
-        Box::<count::Count>::default() as Box<dyn AggregationFunction>,
-    );
-    agg.insert(
-        "count_if".to_string(),
-        Box::<count::CountIf>::default() as Box<dyn AggregationFunction>,
-    );
-    agg.insert(
-        "distinct_count".to_string(),
-        Box::<count::DistinctCount>::default() as Box<dyn AggregationFunction>,
-    );
-    agg.insert(
-        "sum".to_string(),
-        Box::<sum::Sum>::default() as Box<dyn AggregationFunction>,
-    );
-    agg.insert(
-        "avg".to_string(),
-        Box::<sum::Avg>::default() as Box<dyn AggregationFunction>,
-    );
-    agg.insert(
-        "mean".to_string(),
-        Box::<sum::Avg>::default() as Box<dyn AggregationFunction>,
-    );
-    agg.insert(
-        "min".to_string(),
-        Box::<min_max::Min>::default() as Box<dyn AggregationFunction>,
-    );
-    agg.insert(
-        "max".to_string(),
-        Box::<min_max::Max>::default() as Box<dyn AggregationFunction>,
-    );
-    agg.insert(
-        "least".to_string(),
-        Box::<min_max::Min>::default() as Box<dyn AggregationFunction>,
-    );
-    agg.insert(
-        "greatest".to_string(),
-        Box::<min_max::Max>::default() as Box<dyn AggregationFunction>,
-    );
-    agg.insert(
-        "min_by".to_string(),
-        Box::<min_max::MinBy>::default() as Box<dyn AggregationFunction>,
-    );
-    agg.insert(
-        "max_by".to_string(),
-        Box::<min_max::MaxBy>::default() as Box<dyn AggregationFunction>,
-    );
-    agg.insert(
-        "every".to_string(),
-        Box::<all_any::All>::default() as Box<dyn AggregationFunction>,
-    );
-    agg.insert(
-        "any".to_string(),
-        Box::<all_any::Any>::default() as Box<dyn AggregationFunction>,
-    );
-    agg.insert(
-        "some".to_string(),
-        Box::<all_any::Any>::default() as Box<dyn AggregationFunction>,
-    );
-    agg.insert(
-        "first".to_string(),
-        Box::<first_last::First>::default() as Box<dyn AggregationFunction>,
-    );
-    agg.insert(
-        "last".to_string(),
-        Box::<first_last::Last>::default() as Box<dyn AggregationFunction>,
-    );
-    agg.insert(
-        "first_value".to_string(),
-        Box::<first_last::First>::default() as Box<dyn AggregationFunction>,
-    );
-    agg.insert(
-        "last_value".to_string(),
-        Box::<first_last::Last>::default() as Box<dyn AggregationFunction>,
-    );
-    agg.insert(
-        "array_agg".to_string(),
-        Box::<array_agg::ArrayAgg>::default() as Box<dyn AggregationFunction>,
-    );
-    agg.insert(
-        "collect_list".to_string(),
-        Box::<array_agg::ArrayAgg>::default() as Box<dyn AggregationFunction>,
-    );
-    agg.insert(
-        "collect_set".to_string(),
-        Box::<array_agg::SetAgg>::default() as Box<dyn AggregationFunction>,
-    );
-    agg.insert(
-        "array_agg_if".to_string(),
-        Box::<array_agg::ArrayAggIf>::default() as Box<dyn AggregationFunction>,
-    );
+    agg.insert("count".to_string(), Box::<count::Count>::default() as Box<dyn AggregationFunction>);
+    agg.insert("count_if".to_string(), Box::<count::CountIf>::default() as Box<dyn AggregationFunction>);
+    agg.insert("distinct_count".to_string(), Box::<count::DistinctCount>::default() as Box<dyn AggregationFunction>);
+    agg.insert("sum".to_string(), Box::<sum::Sum>::default() as Box<dyn AggregationFunction>);
+    agg.insert("avg".to_string(), Box::<sum::Avg>::default() as Box<dyn AggregationFunction>);
+    agg.insert("mean".to_string(), Box::<sum::Avg>::default() as Box<dyn AggregationFunction>);
+    agg.insert("min".to_string(), Box::<min_max::Min>::default() as Box<dyn AggregationFunction>);
+    agg.insert("max".to_string(), Box::<min_max::Max>::default() as Box<dyn AggregationFunction>);
+    agg.insert("least".to_string(), Box::<min_max::Min>::default() as Box<dyn AggregationFunction>);
+    agg.insert("greatest".to_string(), Box::<min_max::Max>::default() as Box<dyn AggregationFunction>);
+    agg.insert("min_by".to_string(), Box::<min_max::MinBy>::default() as Box<dyn AggregationFunction>);
+    agg.insert("max_by".to_string(), Box::<min_max::MaxBy>::default() as Box<dyn AggregationFunction>);
+    agg.insert("every".to_string(), Box::<all_any::All>::default() as Box<dyn AggregationFunction>);
+    agg.insert("any".to_string(), Box::<all_any::Any>::default() as Box<dyn AggregationFunction>);
+    agg.insert("some".to_string(), Box::<all_any::Any>::default() as Box<dyn AggregationFunction>);
+    agg.insert("first".to_string(), Box::<first_last::First>::default() as Box<dyn AggregationFunction>);
+    agg.insert("last".to_string(), Box::<first_last::Last>::default() as Box<dyn AggregationFunction>);
+    agg.insert("first_value".to_string(), Box::<first_last::First>::default() as Box<dyn AggregationFunction>);
+    agg.insert("last_value".to_string(), Box::<first_last::Last>::default() as Box<dyn AggregationFunction>);
+    agg.insert("array_agg".to_string(), Box::<array_agg::ArrayAgg>::default() as Box<dyn AggregationFunction>);
+    agg.insert("collect_list".to_string(), Box::<array_agg::ArrayAgg>::default() as Box<dyn AggregationFunction>);
+    agg.insert("collect_set".to_string(), Box::<array_agg::SetAgg>::default() as Box<dyn AggregationFunction>);
+    agg.insert("array_agg_if".to_string(), Box::<array_agg::ArrayAggIf>::default() as Box<dyn AggregationFunction>);
     agg
 }
